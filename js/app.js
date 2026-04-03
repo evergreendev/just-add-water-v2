095893997f46091d3ebb1d38dcfba686eb9f0cc9
Joe simpson <joe@egmrc.com>
2026-04-03 11:41:49 -0600

    Add "Contact Your Legislators" page and link from homepage; integrate interactive FAQ functionality

--- js/app.js
+++ js/app.js
@@ -21,4 +21,14 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  const faqItems = document.querySelectorAll('.faq-item');
+  faqItems.forEach(item => {
+    const question = item.querySelector('h3');
+    if (question) {
+      question.addEventListener('click', () => {
+        item.classList.toggle('is-open');
+      });
+    }
+  });
 });
